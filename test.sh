50efdb10f9d89973b93f44333633d53c37d0a09c
Derek Bender <170351+djbender@users.noreply.github.com>
2021-01-17 14:31:26 -0600

    add test script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -eux
+
+docker run "${IMAGE_ID:-ghcr.io/djbender/youtube-dl}" \
+  -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' \
+  --add-metadata \
+  --all-subs \
+  --embed-subs \
+  --embed-thumbnail \
+  https://www.youtube.com/watch?v=g91WrNwUlRY
